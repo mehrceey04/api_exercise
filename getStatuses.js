34d36ff4155c6f7bb86ba20370d52ca025f9139e
robocopkaka <robocopkaka@gmail.com>
2016-10-18 14:19:37 +0100

    Switched to NYT's API. Can retrieve bestsellers either based on an author's name or retrieves all

--- getStatuses.js
+++ getStatuses.js
@@ -1,17 +1,32 @@
 var readlineSync = require('readline-sync');
-var qs = require('querystring'),
-                  oauth=
-                  {
-                  	consumer_key: "8qwH0vaKFlMYoQzF5dEkBwc0E",
-                  	consumer_secret: "FoTGqCRCfqaLNGHYK0lWGLutwo8ampLIM6SfQmfedXm4rOPqiN",
-                  	token: "103872556-Rkwl6DYs23K9qkIGmFGHHNwdCmjrk2stjmWJsYUR",
-                  	token_secret: "wzQRlU6X8gIdzJ7RA97Bb7xnVTjaXezGY74Ul3fwC9k2g",
-                  	'oauth-version':1.0
-                  },url="https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + handle;
-var request = require('request');
-var handle = readlineSync.question("Enter the Twitter handle of the user you want to retrieve tweets for: ");
+var request = require('request')
+var option = readlineSync.question("If you want to retrieve all best-sellers, type all. If you want to retrieve best-sellers from a particular author, type author: ");
 
-request.get({url:url, oauth:oauth}, function(err,res,body){
-	if (err) console.log(err);
-	else console.log(body);
-})
+if(option == "author"){
+   var author = readlineSync.question("Enter an author's name: ");
+   //Use the request method to fetch 
+   request.get({
+   url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json",
+   qs:{
+    'api-key':"e8d78bf70d704f218a49195e256d25b2",
+    author:"" + author  
+   }}, function(err, response, body) {
+   body = JSON.parse(body);
+   console.log(body);
+  })
+}
+else if (option === "all") {
+      //Use the request method to fetch 
+      request.get({
+        url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json",
+      qs:{
+          'api-key':"e8d78bf70d704f218a49195e256d25b2"
+      }}, function(err, response, body) {
+        body = JSON.parse(body);
+        console.log(body);
+      })
+}
+
+else{
+      console.log("You didn't type a valid answer, please try again");
+}
