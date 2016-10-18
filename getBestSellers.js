6cd1b9fb4f2642955ed49b618e8d8f751ab1c51a
robocopkaka <robocopkaka@gmail.com>
2016-10-18 16:35:34 +0100

    Removed API key

--- getBestSellers.js
+++ getBestSellers.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var readlineSync = require('readline-sync');
 var request = require('request')
 var option = readlineSync.question("If you want to retrieve all best-sellers, type all. If you want to retrieve best-sellers from a particular author, type author: ");
@@ -8,7 +9,7 @@ if(option == "author"){
    request.get({
    url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json",
    qs:{
-    'api-key':"e8d78bf70d704f218a49195e256d25b2",
+    'api-key':process.env.API_KEY,
     author:"" + author  
    }}, function(err, response, body) {
    body = JSON.parse(body);
@@ -20,7 +21,7 @@ else if (option === "all") {
       request.get({
         url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json",
       qs:{
-          'api-key':"e8d78bf70d704f218a49195e256d25b2"
+          'api-key':process.env.API_KEY
       }}, function(err, response, body) {
         body = JSON.parse(body);
         console.log(body);
